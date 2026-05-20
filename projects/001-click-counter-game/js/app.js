a66c5e2f325e561264d02f398433728456dc2c2e
EAtrey <eamoore21@gmail.com>
2026-05-19 21:54:59 -0400

    add: ui classes, add level ui and logic, move counter to header

--- projects/001-click-counter-game/js/app.js
+++ projects/001-click-counter-game/js/app.js
@@ -1,10 +1,15 @@
 let theButton = document.getElementById('click-me');
-let feedback = document.getElementById('feedback');
+let feedbackValue = document.getElementById('feedback-value');
+let levelValue = document.getElementById('level-current');
+let levelProgress = document.getElementById('level-progress');
 let clicks = 0;
+let clicksToLevelUp = 10;
+let level = 0;
 
 let handleClick = () => {
     addClick();
     updateFeedback();
+    progressLevel();
 }
 
 let addClick = () => {
@@ -12,7 +17,20 @@ let addClick = () => {
 }
 
 let updateFeedback = () => {
-feedback.innerHTML = `${clicks} Clicks`
+    feedbackValue.innerHTML = `${clicks}`;
+}
+
+let progressLevel = () => {
+    if(levelProgress.getAttribute('value') < clicksToLevelUp) {
+        levelProgress.setAttribute('value', clicks);
+    } else {
+        level+=1;
+        clicksToLevelUp = clicksToLevelUp * level;
+        levelValue.innerHTML = `${level}`;
+        levelProgress.setAttribute('value', 0);
+        levelProgress.setAttribute('max', clicksToLevelUp);
+    }
+    
 }
 
 theButton.addEventListener('click', handleClick);
